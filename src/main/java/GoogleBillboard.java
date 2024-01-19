fc9955b0f77de859a1855d6dcea2db7410669e03
carrietam123 <157089313+carrietam123@users.noreply.github.com>
2024-01-18 21:19:29 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -3,29 +3,26 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		int a = 0;
-    boolean yes = false;
-
-    while (a + 10 <= e.length() && !yes) {
-        String digits = e.substring(a, a + 10);
+		boolean primeFound = false;
+    
+    for (int i = 2; i <= e.length() - 10 && !primeFound; i++) {
+        String digits = e.substring(i, i + 10);
         double dNum = Double.parseDouble(digits);
-
+        
         if (isPrime(dNum)) {
-            System.out.println(digits);
-            yes = true;
+            System.out.println("First 10-digit prime found: " + dNum);
+            primeFound = true;
         }
-        a++;
     }
 	}
 	
-	public static boolean isPrime(double d){
-		if(d < 2){
-    			return false;
-		}
-  		for(int i = 2; i <= Math.sqrt(d); i++)
-    			if(d % i == 0)
-      				return false;
-  		return true;
+	public static boolean isPrime(double num){
+		if (num < 2)
+        return false;
+    for (int i = 2; i <= Math.sqrt(num); i++)
+        if (num % i == 0)
+            return false;
+    return true;
 	}
 }
 
