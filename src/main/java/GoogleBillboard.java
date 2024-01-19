1d7f1624e0e9c9ec8529f165a0c2c28e99de4a8f
carrietam123 <157089313+carrietam123@users.noreply.github.com>
2024-01-18 21:28:55 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,15 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		boolean primeFound = false;
+		boolean prime = false;
     
-    for (int i = 2; i <= e.length() - 10 && !primeFound; i++) {
+    for (int i = 2; i <= e.length() - 10 && !prime; i++) {
         String digits = e.substring(i, i + 10);
         double dNum = Double.parseDouble(digits);
         
-        if (isPrime(dNum)) {
+        if (prime(dNum)) {
             System.out.println(dNum);
-            primeFound = true;
+            isprime = true;
         }
     }
 	}
